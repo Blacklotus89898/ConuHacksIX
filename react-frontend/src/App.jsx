90199af8e1dfca6f8cd7c58b3ff58f2ff8b10270
Blacklotus89898 <song.yang.chen89898@gmail.com>
2025-02-02 01:46:56 -0500

    screenShare, weather and container update

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Sandbox } from './components/sandbox'
+import { Container } from './components/container'
+import Download from './components/download'
+import Chat from './components/chat'
+import ChatGPTRequest from './components/gpt'
+import Weather from './components/weather'
+import ScreenShare from './components/screenShare'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Sandbox></Sandbox>
-      
+      <Container>
+        <Sandbox></Sandbox>
+      </Container>
+      <Container>
+        <Download></Download>
+      </Container>
+      <Container>
+        <Chat></Chat>
+      </Container>
+      <Container>
+        <Weather></Weather>
+      </Container>
+      <Container>
+        <ChatGPTRequest></ChatGPTRequest>
+      </Container>
+
+      <Container>
+        <ScreenShare></ScreenShare>
+      </Container>
+
     </>
   )
 }
